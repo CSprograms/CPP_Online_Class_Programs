192248a564ac49ec40a15a3ed1fa3172ab00d460
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-24 15:08:10 +0530

    Create 05-Online-Class.cpp

---
+++ 05-Online-Class.cpp
@@ -0,0 +1,18 @@
+// Arithmetic Operations on double variables | Binary and Unary
+#include<iostream>
+using namespace std;
+int main()
+{
+    double ip1, ip2;
+
+    cout<<"Enter value for ip1 : ";     cin>>ip1;
+    cout<<"Enter value for ip2 : ";     cin>>ip2;
+
+    cout<<"-ip1 : "<<-ip1<<endl;
+    cout<<"ip1 + ip2 : "<<ip1+ip2<<endl;
+    cout<<"ip1 - ip2 : "<<ip1-ip2<<endl;
+    cout<<"ip1 * ip2 : "<<ip1*ip2<<endl;
+    cout<<"ip1 / ip2 : "<<ip1/ip2<<endl;
+
+return 0;
+}
