de04656eb0b8e27111ca3cbfe9527c5be2d38660
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-24 15:07:25 +0530

    Create 03-Online-Class.cpp

---
+++ 03-Online-Class.cpp
@@ -0,0 +1,20 @@
+// Arithmetic Operations on int variables | Binary and Unary
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ip1, ip2;
+
+    cout<<"Enter value for ip1 : ";     cin>>ip1;
+    cout<<"Enter value for ip2 : ";     cin>>ip2;
+
+    cout<<"-ip1 : "<<-ip1<<endl;
+    cout<<"ip1 + ip2 : "<<ip1+ip2<<endl;
+    cout<<"ip1 - ip2 : "<<ip1-ip2<<endl;
+    cout<<"ip1 * ip2 : "<<ip1*ip2<<endl;
+    cout<<"ip1 / ip2 : "<<ip1/ip2<<endl;
+    cout<<"ip1 % ip2 : "<<ip1%ip2<<endl;
+
+
+return 0;
+}
