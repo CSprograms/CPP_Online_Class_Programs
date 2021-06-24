768f8734f8cfd26f3af51f2840db8356792d3fd6
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-24 15:09:13 +0530

    Create 06-Online-Class.cpp

---
+++ 06-Online-Class.cpp
@@ -0,0 +1,18 @@
+// Simple if -- Odd or Even
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ip1=8;
+
+    //cout<<"Enter value for ip1 : ";     cin>>ip1;
+
+    if(ip1%2 == 1)
+        cout<<"The Given Value is ODD"<<endl;
+
+    if(ip1%2 == 0)
+        cout<<"The Given Value is EVEN"<<endl;
+
+
+return 0;
+}
