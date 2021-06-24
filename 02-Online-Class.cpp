944e7a5f6b27f0730580bdbf39d68976fa95e505
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-24 15:07:13 +0530

    Create 02-Online-Class.cpp

---
+++ 02-Online-Class.cpp
@@ -0,0 +1,25 @@
+// I/O Operations on different types of variables
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Creating Variables for different basic data type.
+    char ip1;
+    int ip2;
+    float ip3;
+    double ip4;
+
+    //Input
+    cout<<"Enter a char : ";    cin>>ip1;
+    cout<<"Enter a int : ";     cin>>ip2;
+    cout<<"Enter a float : ";   cin>>ip3;
+    cout<<"Enter a double : ";  cin>>ip4;
+
+    //Output
+    cout<<"ip1 : "<<ip1<<endl;
+    cout<<"ip2 : "<<ip2<<endl;
+    cout<<"ip3 : "<<ip3<<endl;
+    cout<<"ip4 : "<<ip4<<endl;
+
+return 0;
+}
