ff4a96e943193448a1d7d28ec679ac6ef583e50c
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-24 15:06:52 +0530

    Create 01-Online-Class.cpp

---
+++ 01-Online-Class.cpp
@@ -0,0 +1,23 @@
+// sizeof() Operation on different types of variables
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Creating Variables for different basic data type.
+    char ip1;
+    int ip2;
+    float ip3;
+    double ip4;
+
+    cout<<"sizeof(ip1) : "<<sizeof(ip1)<<endl;
+    cout<<"sizeof(ip2) : "<<sizeof(ip2)<<endl;
+    cout<<"sizeof(ip3) : "<<sizeof(ip3)<<endl;
+    cout<<"sizeof(ip4) : "<<sizeof(ip4)<<endl;
+
+return 0;
+}
+
+// negative 2^n-1
+// positive (2^n-1)-1
+
+//unsigned (positive) (2^n)-1
