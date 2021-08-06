d8345249729908bb3b57b7c62378f5a4c3e74cc6
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-08-06 08:24:48 +0530

    Create 21a-Online-Class.cpp

---
+++ 21a-Online-Class.cpp
@@ -0,0 +1,15 @@
+// Square
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int ip;
+
+    cout<<"Enter a integer value : ";
+    cin>>ip;
+
+    cout<<ip<<"^2 : "       <<ip*ip     <<endl;
+
+return 0;
+}
